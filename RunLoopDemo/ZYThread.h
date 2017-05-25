cedd32363ae31be1741a789e5a1c48183508f00a
apple <776210576@qq.com>
2017-05-25 11:58:59 +0800

    first commit

---
+++ RunLoopDemo/ZYThread.h
@@ -0,0 +1,13 @@
+//
+//  ZYThread.h
+//  RunLoopDemo
+//
+//  Created by apple on 2017/5/25.
+//  Copyright © 2017年 ZY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZYThread : NSThread
+
+@end
---
+++ RunLoopDemo/ZYThread.m
@@ -0,0 +1,13 @@
+//
+//  ZYThread.m
+//  RunLoopDemo
+//
+//  Created by apple on 2017/5/25.
+//  Copyright © 2017年 ZY. All rights reserved.
+//
+
+#import "ZYThread.h"
+
+@implementation ZYThread
+
+@end
